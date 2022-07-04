4ce518347c03ab9495a1d433cc481a68efcb9500
wzj19952545705 <wzj19952545705@126.com>
2022-07-04 20:17:45 +0800

    my emacs config, just a beginning

---
+++ .emacs.d/elpa/doom-modeline-20220629.1718/doom-modeline-pkg.el
@@ -0,0 +1,14 @@
+(define-package "doom-modeline" "20220629.1718" "A minimal and modern mode-line"
+  '((emacs "25.1")
+    (compat "28.1.1.1")
+    (shrink-path "0.2.0"))
+  :commit "26557338fdcadd2c081d7464c59859ba46c3ae82" :authors
+  '(("Vincent Zhang" . "seagle0128@gmail.com"))
+  :maintainer
+  '("Vincent Zhang" . "seagle0128@gmail.com")
+  :keywords
+  '("faces" "mode-line")
+  :url "https://github.com/seagle0128/doom-modeline")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ .emacs.d/elpa/evil-20220701.1529/evil-pkg.el
@@ -0,0 +1,12 @@
+(define-package "evil" "20220701.1529" "Extensible Vi layer for Emacs."
+  '((emacs "24.1")
+    (goto-chg "1.6")
+    (cl-lib "0.5"))
+  :commit "59158b3df13681c42edf5ba73e307adef503777a" :maintainer
+  '("Tom Dalziel" . "tom.dalziel@gmail.com")
+  :keywords
+  '("emulation" "vim")
+  :url "https://github.com/emacs-evil/evil")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
